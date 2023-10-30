d1574ae2e4a62421775bccb6facbc732d599cd3d
Nitya Joshi <nitya.joshi.99@gmail.com>
2023-10-30 10:14:24 -0500

    added separate pretrained files with and without BN

--- _tutorials/src/evaluation.jl
+++ _tutorials/src/evaluation.jl
@@ -1,4 +1,4 @@
-function evaluate_submission(model, data, simulation_length=10000)
+function evaluate_submission(model, data, simulation_length=1000)
     @info "Calculating HW cost..."
     power = power_consumption(model, (96, 96, 3, 1))
     area = area_consumption(model, (96, 96, 3, 1))
@@ -8,7 +8,8 @@ function evaluate_submission(model, data, simulation_length=10000)
 
     @info "Evaluating simulated model performance..."
     if(model[1][2] isa BatchNorm)
-        model_scaled = merge_conv_bn(model)
+        #model_scaled = merge_conv_bn(model)
+        throw(ArgumentError("Model must not have BatchNorms anymore.\n Merge BatchNorms, re-train and re-evaluate."))
     else
         model_scaled = deepcopy(model)
     end
@@ -36,31 +37,4 @@ function evaluate_submission(model, simulation_length=1000)
     valaug = map_augmentation(ImageToTensor(), valdata)
     valloader = DataLoader(BatchView(valaug; batchsize =  2*32); buffer = true)
     return evaluate_submission(model, valloader, simulation_length)
-end
-
-function evaluate_submission_accuracy(model, data, simulation_length)
-
-    accfn(ŷ::AbstractArray, y::AbstractArray) = mean((ŷ .> 0) .== y)
-    accfn(data, model) = mean(accfn(model(x), y) for (x, y) in data)
-
-    @info "Evaluating simulated model performance..."
-    if(model[1][2] isa BatchNorm)
-        model_scaled = merge_conv_bn(model)
-    else
-        model_scaled = deepcopy(model)
-    end
-    #total_scaling = prod(prod.(scalings))
-    add_conversion_error!(model_scaled, simulation_length)
-    #model_rescaled = Chain(model_scaled, x -> x .* total_scaling)
-    acc = accfn(data, model_scaled)
-
-    return acc
-end
-
-function evaluate_submission_accuracy(model, simulation_length=10000)
-    dataroot = joinpath(artifact"vww", "vww-hackathon")
-    valdata = VisualWakeWords(dataroot; subset = :val)
-    valaug = map_augmentation(ImageToTensor(), valdata)
-    valloader = DataLoader(BatchView(valaug; batchsize =  2*32); buffer = true)
-    return evaluate_submission_accuracy(model, valloader, simulation_length)
 end
